e1ee91d75973251b2a954b4d411051fef309a35b
Horncn <Mkhrlov@gmail.com>
2019-09-24 01:31:01 +0300

    Attempt to solve by deleting

--- lab_1/main.py
+++ lab_1/main.py
@@ -48,43 +48,4 @@ def get_top_n(frequencies: dict, top_n: int) -> tuple:
     return best
 
 
-def read_from_file(path_to_file: str, lines_linit: int) -> str:
-    file = open(path_to_file)
-    counter = 0
-    text = ''
-    for line in file:
-        text += line
-        counter += 1
-        if counter == lines_linit:
-            break
-    file.close()
-    return text
 
-
-def write_to_file(path_to_file: str, content: tuple):
-    file = open(path_to_file, 'w')
-    for i in content:
-        file.write(i)
-        file.write('\n')
-    file.close()
-
-
-stop_words = ('ourselves', 'hers', 'between', 'yourself', 'but', 'again', 'there', 'about',
-              'once', 'during', 'out', 'very', 'having', 'with', 'they', 'own', 'an', 'be',
-              'some', 'for', 'do', 'its', 'yours', 'such', 'into', 'of', 'most', 'itself',
-              'other', 'off', 'is', 's', 'am', 'or', 'who', 'as', 'from', 'him', 'each', 'the',
-              'themselves', 'until', 'below', 'are', 'we', 'these', 'your', 'his', 'through',
-              'don', 'nor', 'me', 'were', 'her', 'more', 'himself', 'this', 'down', 'should',
-              'our', 'their', 'while', 'above', 'both', 'up', 'to', 'ours', 'had', 'she', 'all',
-              'no', 'when', 'at', 'any', 'before', 'them', 'same', 'and', 'been', 'have', 'in',
-              'will', 'on', 'does', 'yourselves', 'then', 'that', 'because', 'what', 'over',
-              'why', 'so', 'can', 'did', 'not', 'now', 'under', 'he', 'you', 'herself', 'has',
-              'just', 'where', 'too', 'only', 'myself', 'which', 'those', 'i', 'after', 'few',
-              'whom', 't', 'being', 'if', 'theirs', 'my', 'against', 'a', 'by', 'doing',
-              'it', 'how', 'further', 'was', 'here', 'than')
-
-text = read_from_file('Data.txt', 6)
-dictionary = calculate_frequences(text)
-dictionary = filter_stop_words(dictionary, stop_words)
-rating = get_top_n(dictionary, 10)
-write_to_file('report.txt', rating)
