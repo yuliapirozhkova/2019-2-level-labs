ec397ca520e57e2ba16f5f7f3a4fe36536551e7e
nelpomen <marnelyubina@gmail.com>
2019-09-23 17:24:03 +0300

    Changed function calculate_frequences

--- lab_1/main.py
+++ lab_1/main.py
@@ -2,13 +2,23 @@
 Labour work #1
 Count frequencies dictionary by the given arbitrary text
 """
-
-
-def calculate_frequences(text: str) -> dict:
-    """
-    Calculates number of times each word appears in the text
-    """
-    print('KFJ')
+import re
+def calculate_frequences(text: str):
+    text = text.lower()
+    list_text_all = re.split('[ "#\'\[\]!?:;,.\n\-...^*+~]', text)
+    while '' in list_text_all: list_text_all.remove('')
+    # print (list_text_all)
+    list_text = []
+    for i in range(len(list_text_all)):
+        if list_text_all[i].isalpha() == True:
+            list_text.append(list_text_all[i])
+    # print (list_text)
+    list_freq = []
+    for i in range(len(list_text)):
+        list_freq.append(list_text.count(list_text[i]))
+    # print (list_freq)
+    dict = {list_text[i] : list_freq[i] for i in range(len(list_text))}
+    return dict
 
 def filter_stop_words(frequencies: dict, stop_words: tuple) -> dict:
     """
