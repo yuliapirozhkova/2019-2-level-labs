449bd660a9a8c676c8ea93a1d25d80e216e822ed
Horncn <Mkhrlov@gmail.com>
2019-09-24 11:24:30 +0300

    added docstrings

--- lab_1/main.py
+++ lab_1/main.py
@@ -1,4 +1,13 @@
+"""
+Labour work #1
+Count frequencies dictionary by the given arbitrary text
+"""
+
+
 def calculate_frequences(text: str) -> dict:
+    """
+    Calculates number of times each word appears in the text
+    """
     frequencies = {}
     new_text = ''
     if text is None:
@@ -21,6 +30,9 @@ def calculate_frequences(text: str) -> dict:
 
 
 def filter_stop_words(frequencies: dict, stop_words: tuple) -> dict:
+    """
+    Removes all stop words from the given frequencies dictionary
+    """
     if frequencies is None:
         frequencies = {}
         return frequencies
@@ -38,6 +50,9 @@ def filter_stop_words(frequencies: dict, stop_words: tuple) -> dict:
 
 
 def get_top_n(frequencies: dict, top_n: int) -> tuple:
+    """
+    Takes first N popular words
+    """
     if not isinstance(top_n, int):
         frequencies = ()
         return frequencies
