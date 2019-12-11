bdd50e4fe240d18a330e85800b09e0b1eb8a268b
yuliapirozhkova <pirozhkova1010@gmail.com>
2019-12-11 13:55:28 +0300

    upd2

--- lab_3/main.py
+++ lab_3/main.py
@@ -33,14 +33,16 @@ class WordStorage:
             for word, i in self.storage.items():
                 if i == idd:
                     return word
-        return 'UNK'
+        else:
+            return 'UNK'
 
     def from_corpus(self, corpus: tuple):
-        if isinstance(corpus, tuple):
-            for word in corpus:
-                self.put(word)
-            return self.storage
-        return {}
+    #    if isinstance(corpus, tuple):
+        for word in corpus:
+            self.put(word)
+        return self.storage
+   # else:
+    #    return {}
 
 
 class NGramTrie:
@@ -82,43 +84,42 @@ class NGramTrie:
                 self.gram_log_probabilities[gram] = math.log(self.gram_frequencies[gram]/prefixes[gram[:-1]])
 
     def predict_next_sentence(self, prefix: tuple) -> list:
-        if prefix is None or not isinstance(prefix, tuple) or len(prefix) + 1 != self.size:
+        word_1 = []
+        if not isinstance(prefix, tuple) or len(prefix) + 1 != self.size:
             return []
-        list_of_keys = list(self.gram_log_probabilities.keys())
-        new_sentence = list(prefix)
+        final = list(prefix)
         while True:
-            new_list = []
-            for gram in list_of_keys:
-                if gram[:-1] == prefix:
-                    new_list.append(self.gram_log_probabilities[gram])
-            if not new_list:
+            prob = []
+            for n_gram in list(self.gram_log_probabilities.keys()):
+                if n_gram[:-1] == prefix:
+                    prob.append(self.gram_log_probabilities[n_gram])
+            if not prob:
                 break
-            new_word = max(new_list)
+            log_prob = max(prob)
             for word, probability in list(self.gram_log_probabilities.items()):
-                if new_word == probability:
-                    new_word = word[-1]
-            new_sentence.append(new_word)
-            new_prefix = list(prefix[1:])
-            new_prefix.append(new_word)
-            prefix = tuple(new_prefix)
-        return new_sentence
-
+                if log_prob == probability:
+                    word_1 = word[-1]
+            final.append(word_1)
+            pref_1 = list(prefix[1:])
+            pref_1.append(word_1)
+            prefix = tuple(pref_1)
+        return final
 
 def encode(storage_instance, corpus) -> list:
     coded = []
-    for _ in corpus:
+    for l in corpus:
         for_coded = []
-        for word in _:
+        for word in l:
             word = storage_instance.get_id_of(word)
             for_coded.append(word)
-        coded.append(coded)
+        coded.append(for_coded)
     return coded
 
 
 def split_by_sentence(text: str) -> list:
     if isinstance(text, str) and text is not '' and len(text) > 1 and len(text.split(' ')) > 1 and text is not None:
         new_text = ''
-        text = text.replace('\n', '')
+        text = text.replace('\n', ' ')
         text = text.replace('  ', ' ')
         text = text.replace('!', '.')
         text = text.replace('?', '.')
@@ -132,8 +133,8 @@ def split_by_sentence(text: str) -> list:
                     symbol = 'sep'+symbol
                 new_text += symbol.lower()
             sentences_1 = new_text.split('sep')
-        while '' in sentences_1:
-            sentences_1.remove('')
+       # while '' in sentences_1:
+        #    sentences_1.remove('')
         for i in sentences_1:
             i = i.split()
             i.insert(0, '<s>')
@@ -143,32 +144,33 @@ def split_by_sentence(text: str) -> list:
     return []
 
 
-WSt = WordStorage()
-NGr = NGramTrie(2)
-sentences = split_by_sentence(REFERENCE_TEXT)
-for sent in sentences:
-    for word_ in sent:
-        WSt.put(word_)
-sentences1 = encode(WSt, sentences)
-for sent in sentences1:
-    NGr.fill_from_sentence(tuple(sent))
-NGr.calculate_log_probabilities()
-prefix1 = 'she'
-pref_lst = prefix1.split()
-pref_num = []
-for pref in pref_lst:
-    pref_num.append(WSt.get_id_of(pref))
-numbers_res = NGr.predict_next_sentence(tuple(pref_num))
-fin = []
-for number in numbers_res:
-    fin.append(WSt.get_original_by(number))
-print(fin)
+# WSt = WordStorage()
+# NGr = NGramTrie(2)
+# sentences = split_by_sentence((''' Web Authentication
+# Login Successful !
+# You can now use all regular network services over the wireless network.
+#
+# Please retain this small logout window in order to logoff when done. Note that you can always use the following URL to retrieve this page:
+# https://192.0.2.1/logout.html'''))
+# for sent in sentences:
+#     WSt.from_corpus(sent)
+# sentences1 = encode(WSt, sentences)
+# for sent in sentences1:
+#     NGr.fill_from_sentence(tuple(sent))
+# NGr.calculate_log_probabilities()
+# prefix1 = 'use'
+# pref_lst = prefix1.split()
+# pref_num = []
+# for pref in pref_lst:
+#     pref_num.append(WSt.get_id_of(pref))
+# numbers_res = NGr.predict_next_sentence(tuple(pref_num,))
+# fin = []
+# for number in numbers_res:
+#     fin.append(WSt.get_original_by(number))
+# print(fin)
 
 
 
-#  id_of_word = WS.get_id_of(word_train)
-# final = NGR.predict_next_sentence(tuple(id_of_word) )
-#  print(final)
 
 
 
