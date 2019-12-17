7d0d4bb33dc5272d24421aea89a8e30a99de37aa
yuliapirozhkova <pirozhkova1010@gmail.com>
2019-12-17 12:52:10 +0300

    final

--- lab_4/main.py
+++ lab_4/main.py
@@ -106,5 +106,5 @@ if __name__ == '__main__':
     tf_idf.calculate_tf()
     tf_idf.calculate_idf()
     tf_idf.calculate()
-    print(tf_idf.report_on('зебра', 0))
-    print(tf_idf.report_on('дельфин', 1))
+    print(tf_idf.report_on('good', 0))
+    print(tf_idf.report_on('and', 1))
